4552e555aa3a3200f2d51c97667c1869e3e46060
Jonathan Raya Rios <jonathan17699@hotmail.es>
2022-07-09 21:18:29 +0200

    Añadir categorias terminado

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+
 
 export const GifExpertApp = () => {
 
-  return (
-    <>
-        <h1>GifExpertApp</h1>
-    </>
-  )
+	const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
+	const onAddCategory = (newCategory) => {
+		if(categories.includes(newCategory)) {
+			return;
+		}
+
+		setCategories([newCategory, ...categories]);
+	}
+
+	return (
+	<>
+		<h1>GifExpertApp</h1>
+
+		<AddCategory onNewCategory={(value) => onAddCategory(value)} />
+
+		<ol>
+			{
+				categories.map((category) => {
+					return <li key={category}>{category}</li>
+				})
+			}          
+		</ol>
+	</>
+	)
 }
