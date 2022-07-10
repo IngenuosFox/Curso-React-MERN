237ddf0b3466d73e6b33143a1eb5b5090eb07373
Jonathan Raya Rios <jonathan17699@hotmail.es>
2022-07-10 12:52:41 +0200

    GIFs visibles en la web

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,18 +1,31 @@
 import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs"
+import { GifItem } from "./GifItem";
 
 
 export const GifGrid = ({category}) => {
 
-    const [counter, setCounter] = useState(10);
+    const [images, setImages] = useState([]);
+
+    const getImages = async() => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    }
 
     useEffect(() => {
-        getGifs(category);
+        getImages();
     }, []);
 
     return (
         <>
             <h3>{category}</h3>
+            <div className="card-grid">
+                {
+                    images.map((image) => (
+                        <GifItem key={image.id} {...image} />
+                    ))
+                }
+            </div>
         </>
     )
 }
