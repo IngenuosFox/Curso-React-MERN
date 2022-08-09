b9a814b233f3e73e79aaad62bfb6c595a34087d7
Jonathan Raya Rios <jonathan17699@hotmail.es>
2022-08-09 16:55:38 +0200

    Update fileUpload.test.js

--- 08-journal-app/__test__/helpers/fileUpload.test.js
+++ 08-journal-app/__test__/helpers/fileUpload.test.js
@@ -4,7 +4,7 @@ describe('Pruebas en fileUpload', () => {
 	
 	test('debe subir el archivo correctamente a cloudinary', async() => {
 
-		const imageUrl = "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'"
+		const imageUrl = "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
 		const resp = await fetch(imageUrl)
 		const blob = await resp.blob()
 		const file = new File([blob], "foto.jpg")
